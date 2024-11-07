1af8956d46f57394ae6f749a8af2169856bb3f55
Eisss20 <pajenan.j@gmail.com>
2024-11-07 19:26:57 +0700

    Feat : setting node to  deploy data

--- app.mjs
+++ app.mjs
@@ -1,18 +1,12 @@
 import express from "express";
 import cors from "cors";
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4001;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  return res.json({
-    data: "backend is ON",
-  });
-});
-
-app.get("/profiles", (req, res) => {
   return res.json({
     data: {
       name: "john",

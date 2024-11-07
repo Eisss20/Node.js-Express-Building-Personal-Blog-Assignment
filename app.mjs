f1f47e6604b416eab2fff5383c48e8aa30eda9de
Eisss20 <pajenan.j@gmail.com>
2024-11-07 18:53:27 +0700

    Test : test3 the setting package.json

--- app.mjs
+++ app.mjs
@@ -1,16 +1,23 @@
 import express from "express";
 import cors from "cors";
-
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
-
 app.use(express.json());
 
 app.get("/", (_req, res) => {
   return res.json({
-    data: "testing",
+    data: "backend is ON",
+  });
+});
+
+app.get("/profiles", (req, res) => {
+  return res.json({
+    data: {
+      name: "john",
+      age: 20,
+    },
   });
 });
 

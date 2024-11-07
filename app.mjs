fd20eb48bc4a6bbb24950f3a9874e5c0b71cc718
Eisss20 <pajenan.j@gmail.com>
2024-11-07 18:15:09 +0700

    Feat : Add file vercel to deploy

--- app.mjs
+++ app.mjs
@@ -1,7 +1,13 @@
 import express from "express"
+import cors from "cors";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
 
 app.get("/profiles", (req, res) => {
     return res.json({
